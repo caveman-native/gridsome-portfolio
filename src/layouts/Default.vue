677e38a7ea3324af9ce0946a51705d8794a9e47b
steve-visibble <steveobrien73@gmail.com>
2019-08-10 20:31:59 -0400

    edits

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,11 +2,13 @@
   <div class="layout">
     <header class="header">
       <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
+        <g-link to="/">stephenobrien.me</g-link>
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about">About</g-link>
+        <g-link class="nav__link" to="/links">Links</g-link>
+        <g-link class="nav__link" to="/contact">Contact</g-link>
       </nav>
     </header>
     <slot/>
