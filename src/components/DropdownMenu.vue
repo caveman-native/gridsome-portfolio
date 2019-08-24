811e1d85ec14e2d352bde700ba009297d99fe67b
steve-visibble <steveobrien73@gmail.com>
2019-08-24 17:41:27 -0400

    edits

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="relative">
+  <div class="relative test">
 
     <button
-      href="#"
-      class="flex items-center"
-      @click="toggleVisibility"
-      @keydown.space.exact.prevent="toggleVisibility"
-      @keydown.esc.exact="hideDropdown"
-      @keydown.shift.tab="hideDropdown"
-      @keydown.up.exact.prevent="startArrowKeys"
-      @keydown.down.exact.prevent="startArrowKeys"
+        href="#"
+        class="flex items-center"
+        @click="toggleVisibility"
+        @keydown.space.exact.prevent="toggleVisibility"
+        @keydown.esc.exact="hideDropdown"
+        @keydown.shift.tab="hideDropdown"
+        @keydown.up.exact.prevent="startArrowKeys"
+        @keydown.down.exact.prevent="startArrowKeys"
     >
-      <!--<img src="/avatar.jpg" alt="avatar" class="w-8 h-8 rounded-full">-->
-      <font-awesome :icon="['fas', 'bars']"/>
-      <!--
-      <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"></path></svg>-->
+    <font-awesome :icon="['fas', 'bars']"/>
     </button>
+    
     <transition name="dropdown-fade">
       <ul v-on-clickaway="hideDropdown" v-if="isVisible" ref="dropdown" class="absolute normal-case font-normal xs:left-0 lg:right-0 bg-white shadow overflow-hidden rounded w-48 border mt-2 py-1 lg:z-20">
         <li>
@@ -124,43 +122,46 @@
 <script>
 import { mixin as clickaway } from 'vue-clickaway'
 export default {
-  mixins: [ clickaway ],
-  data() {
-    return {
-      isVisible: false,
-      focusedIndex: 0,
-    }
-  },
-  methods: {
+    components: {
+        name: 'DropdownMenu'
+    },
+    mixins: [ clickaway ],
+    data() {
+        return {
+            isVisible: false,
+            focusedIndex: 0,
+        }
+    },
+    methods: {
     toggleVisibility() {
-      this.isVisible = !this.isVisible
+        this.isVisible = !this.isVisible
     },
     hideDropdown() {
-      this.isVisible = false
-      this.focusedIndex = 0
+        this.isVisible = false
+        this.focusedIndex = 0
     },
     startArrowKeys() {
-      if (this.isVisible) {
+        if (this.isVisible) {
         // this.$refs.account.focus()
-        this.$refs.dropdown.children[0].children[0].focus()
-      }
+            this.$refs.dropdown.children[0].children[0].focus()
+        }
     },
     focusPrevious(isArrowKey) {
-      this.focusedIndex = this.focusedIndex - 1
-      if (isArrowKey) {
-        this.focusItem()
-      }
+        this.focusedIndex = this.focusedIndex - 1
+        if (isArrowKey) {
+            this.focusItem()
+        }
     },
     focusNext(isArrowKey) {
-      this.focusedIndex = this.focusedIndex + 1
-      if (isArrowKey) {
-        this.focusItem()
-      }
+        this.focusedIndex = this.focusedIndex + 1
+        if (isArrowKey) {
+            this.focusItem()
+        }
     },
     focusItem() {
-      this.$refs.dropdown.children[this.focusedIndex].children[0].focus()
+        this.$refs.dropdown.children[this.focusedIndex].children[0].focus()
     }
-  }
+    }
 }
 </script>
 
