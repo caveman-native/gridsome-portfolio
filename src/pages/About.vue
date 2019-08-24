e2927948a8976ad8105391d416e82eb01d8a815b
steve-visibble <steveobrien73@gmail.com>
2019-08-24 09:28:18 -0400

    edits

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,16 +1,21 @@
 <template>
-  <Layout>
-      <div class="page-title">About me</div>
-    <div class="">
-          <p class="mt-2 text-gray-200">I am most proficient in the following languages:  HTML, CSS, Javascript and VueJS.</p>
+    <Layout>
+        
+        <div class="page-title">About me</div>
+        
+        <div class="">
+            
+        <p class="mt-2 text-gray-200">I am most proficient in the following languages:  HTML, CSS, Javascript and VueJS.</p>
 
-<p class="mt-2 text-gray-200">My current interests and studies include Javascript, ES6 and the MEVN and MERN Stacks. You might find me poking around in documentation for the following frameworks: NodeJS, ExpressJS, MongoDB, AngularJS, VueJS, ReactJS, NativeScript-Vue, React-Native, NuxtJS, Gridsome, GraphQL, TailwindCSS, Flexbox, CSS Grid, SASS, LESS and Ionic Hybrid Mobile Framework.</p>
+        <p class="mt-2 text-gray-200">My current interests and studies include Javascript, ES6 and the MEVN and MERN Stacks. You might find me poking around in documentation for the following frameworks: NodeJS, ExpressJS, MongoDB, AngularJS, VueJS, ReactJS, NativeScript-Vue, React-Native, NuxtJS, Gridsome, GraphQL, TailwindCSS, Flexbox, CSS Grid, SASS, LESS and Ionic Hybrid Mobile Framework.</p>
 
-<p class="mt-2 text-gray-200">I am currently employed at a clothing retailer as a Front End Web Developer on an ecommerce site. We use the SalesForce Commerce Cloud CMS for managing front end content. Tasks include developing and updating content for our responsive platform. Projects are managed through the use of Git, Bitbucket, Trello and Jira.</p>
+        <p class="mt-2 text-gray-200">I am currently employed at a clothing retailer as a Front End Web Developer on an ecommerce site. We use the SalesForce Commerce Cloud CMS for managing front end content. Tasks include developing and updating content for our responsive platform. Projects are managed through the use of Git, Bitbucket, Trello and Jira.</p>
 
-<p class="mt-2 text-gray-200">I also work with third party tools like Adobe Dynamic Tag Management(DTM) now Adobe Launch, Monetate for A/B Testing and Tealium (TIQ). Tealium TiQ is our current Tag Management System. Writing Javascript and knowledge of site performance and testing is a large part of these tasks.</p>
-</div>
-  </Layout>
+        <p class="mt-2 text-gray-200">I also work with third party tools like Adobe Dynamic Tag Management(DTM) now Adobe Launch, Monetate for A/B Testing and Tealium (TIQ). Tealium TiQ is our current Tag Management System. Writing Javascript and knowledge of site performance and testing is a large part of these tasks.</p>
+
+        </div>
+        
+    </Layout>
 </template>
 
 <script>
@@ -20,3 +25,6 @@ export default {
   }
 }
 </script>
+<style lang="">
+    
+</style>
