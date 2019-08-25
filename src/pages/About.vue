52dce4469023ae11ec00b96176c9e87cd3bc69d1
steve-visibble <steveobrien73@gmail.com>
2019-08-24 20:33:47 -0400

    edits about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,18 +1,35 @@
 <template>
     <Layout>
         
-        <div class="page-title">About me</div>
+        <div class="about-container flex flex-col h-auto mb-12">
         
-        <div class="">
             
-        <p class="mt-2 text-gray-200">I am most proficient in the following languages:  HTML, CSS, Javascript and VueJS.</p>
+        <div class="page-title flex flex-col items-start ml-6 mr-6 mb-8 font-sans text-3xl text-white font-semibold">What I've been up to lately...</div>
+        
+        <div class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-xl text-gray-800 font-normal">Current Employment</div>
+            
+        <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-base text-gray-800 font-normal">Front End Web Developer, Talbots, Inc. HINGHAM MA., August 2011- to Present 8+ yrs</p>
 
-        <p class="mt-2 text-gray-200">My current interests and studies include Javascript, ES6 and the MEVN and MERN Stacks. You might find me poking around in documentation for the following frameworks: NodeJS, ExpressJS, MongoDB, AngularJS, VueJS, ReactJS, NativeScript-Vue, React-Native, NuxtJS, Gridsome, GraphQL, TailwindCSS, Flexbox, CSS Grid, SASS, LESS and Ionic Hybrid Mobile Framework.</p>
+        <hr class="bg-black w-3/4 mt-6 mb-6">
 
-        <p class="mt-2 text-gray-200">I am currently employed at a clothing retailer as a Front End Web Developer on an ecommerce site. We use the SalesForce Commerce Cloud CMS for managing front end content. Tasks include developing and updating content for our responsive platform. Projects are managed through the use of Git, Bitbucket, Trello and Jira.</p>
+        <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-base text-gray-800 font-normal">I am currently employed at a clothing retailer as a Front End Web Developer on an ecommerce site. We use the SalesForce Commerce Cloud CMS for managing front end content. Tasks include developing and updating content for our responsive platform. Projects are managed through the use of Git, Bitbucket, Trello and Jira.</p>
 
-        <p class="mt-2 text-gray-200">I also work with third party tools like Adobe Dynamic Tag Management(DTM) now Adobe Launch, Monetate for A/B Testing and Tealium (TIQ). Tealium TiQ is our current Tag Management System. Writing Javascript and knowledge of site performance and testing is a large part of these tasks.</p>
+        <hr class="bg-black w-3/4 mt-6 mb-6">
 
+        <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-base text-gray-800 font-normal">I also work with third party tools like Adobe Dynamic Tag Management(DTM) now Adobe Launch, Monetate for A/B Testing and Tealium (TIQ). Tealium TiQ is our current Tag Management System. Writing Javascript and knowledge of site performance and testing is a large part of these tasks.</p>
+        
+        <hr class="bg-black w-3/4 mt-6 mb-6">
+
+        <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-xl text-gray-800 font-normal">Most Proficient Programming Experience HTML, CSS, Javascript and VueJS</p>
+        
+        <hr class="bg-black w-3/4 mt-6 mb-6">
+        
+        <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-xl text-gray-800 font-normal">Interests Developing progressive web applications, single page applications and static sites using the JAMStack.</p>
+        
+        <hr class="bg-black w-3/4 mt-6 mb-6">
+        
+        <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-base text-gray-800 font-normal">My current interests and studies include Javascript, ES6 and the MEVN and MERN Stacks. You might find me poking around in documentation for the following frameworks: NodeJS, ExpressJS, MongoDB, AngularJS, VueJS, ReactJS, NativeScript-Vue, React-Native, NuxtJS, Gridsome, GraphQL, TailwindCSS, Flexbox, CSS Grid, SASS, LESS and Ionic Hybrid Mobile Framework.</p>
+        
         </div>
         
     </Layout>
