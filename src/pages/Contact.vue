c270bccb603f11a06d275b87d3095f58de927be8
steve-visibble <steveobrien73@gmail.com>
2019-09-02 14:19:51 -0400

    edit

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -3,8 +3,8 @@
 <div class="about-container flex flex-col h-auto mb-12">
     <div class="page-title flex flex-col items-start ml-6 mr-6 mb-8 font-sans text-3xl text-white font-semibold">Contact me</div>
     <div class="my-form flex flex-col items-start ml-6 mr-6 mb-8 font-sans text-xl text-gray-900">
-        <form name="contact" method="POST" data-netlify="true">
-            
+        
+        <form name="stephenobrienme-contact-form" netlify>
             <div class="sender-info">
                 <div>
                     <label for="name" class="label block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4 rounded">Name</label>
@@ -19,7 +19,6 @@
                 <label for="message" class="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4 rounded">Short Message</label>
                 <textarea name="message"></textarea>
             </div>
-            <div data-netlify-recaptcha="true"></div>
             <div class="submit-btn">
                 <button class="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4" type="submit" name="submit">Submit</button>
             </div>
