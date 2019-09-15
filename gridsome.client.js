5c2248fb574247ae360aa48c1dfa8005322badb8
steve-visibble <steveobrien73@gmail.com>
2019-09-15 12:40:36 -0400

    edit

--- gridsome.client.js
+++ gridsome.client.js
@@ -15,10 +15,11 @@ import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 import { faBlog } from '@fortawesome/free-solid-svg-icons'
 import { faFile } from '@fortawesome/free-solid-svg-icons'
 import { faAt } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcase} from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 config.autoAddCss = false;
-library.add(faGithub, faTwitter, faBars, faHome, faUserCircle, faBlog, faFile, faAt)
+library.add(faGithub, faTwitter, faBars, faHome, faUserCircle, faBlog, faFile, faAt, faBriefcase)
 
 export default function (Vue) {
     Vue.component('font-awesome', FontAwesomeIcon)
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row items-end w-100 h-10 ml-2 mr-2 mb-4 p-1">
     <ul class="flex flex-1 justify-center p-1 text-xs text-gray-700">
-        <li>Stack &nbsp;<a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome<span class="ml-1 mr-1 text-pink-500">|</span></a></li>
+        <li>Stack &nbsp;<a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome<span class="ml-1 mr-1 ">|</span></a></li>
         <li><a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">Tailwind CSS<span class="ml-1 mr-1 text-pink-500">|</span></a></li>
         <li><a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">VueJS 2.0</a></li>
         <li>&nbsp; Hosting<span class="ml-1 mr-1 text-pink-500">|</span><a href="https://www.netlify.com" target="_blank" rel="noopener">Netlify</a></li>
