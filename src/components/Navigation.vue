088980c0130a985bf93325816acb989d20e334d6
steve-visibble <steveobrien73@gmail.com>
2019-09-15 16:26:28 -0400

    edit

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="flex flex-row flex-wrap relative block mx-auto mt-2 mb-2 ml-0 mr-0 h-20">
         <DropdownMenu class="ml-2 mt-6" />
-    <div class="font-sans font-bold ml-12 m-4 text-gray-300 text-xl">
+    <div class="font-sans font-bold m-4 text-gray-300 text-xl">
             <g-link class="font-sans" to="/">{{ siteDomainName }}</g-link>
         </div>
     </nav>
