b5adad3aea487a370e649e585cee8be529205952
steve-visibble <steveobrien73@gmail.com>
2019-09-15 13:23:31 -0400

    edit

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,9 +3,9 @@
         
         <div class="flex flex-col w-100 md:max-w-xl h-auto mx-auto mb-12">
              
-            <div class="flex flex-row items-start ml-6 mr-6 mb-8 font-sans text-2xl text-pink-700 font-semibold"><font-awesome class="mt-1 mr-4" :icon="['fas', 'briefcase']"/> Current Work &amp; Interests</div>
+            <div class="flex flex-row items-start ml-6 mr-6 mb-8 font-sans text-2xl text-pink-700 font-semibold"><font-awesome class="mt-1 mr-4" :icon="['fas', 'briefcase']"/>Employment &amp; Personal Interests</div>
             
-            <div class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-xl text-gray-300 font-normal">Current Employment</div>
+            <div class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-xl text-green-700 font-normal">Current Employment</div>
                 
             <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-base text-gray-300 font-normal">Front End Web Developer, Talbots, Inc. HINGHAM MA., August 2011- to Present 8+ yrs</p>
 
@@ -13,9 +13,24 @@
 
             <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-base text-gray-300 font-normal">I also work with third party tools like Adobe Dynamic Tag Management(DTM) now Adobe Launch, Monetate for A/B Testing and Tealium (TIQ). Tealium TiQ is our current Tag Management System. Writing Javascript and knowledge of site performance and testing is a large part of these tasks.</p>
             
-            <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-xl text-gray-300 font-normal">My Most Proficient Programming Experience is writing HTML, CSS, Javascript and VueJS</p>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Skills</div>
+                    <p class="font-sans text-xl text-green-700 font-semibold">My Most Proficient Programming Experience</p>
+                </div>
+                <div class="px-6 py-1 mb-10">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">HTML5</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">CSS3</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Javascript</span>
+                    <span class="inline-block bg-gray-200 rounded-full m-2 px-3 py-1 text-sm font-semibold text-gray-700">VueJS</span>
+                </div>
+            </div>
             
-            <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-xl text-gray-300 font-normal">Interests Developing progressive web applications, single page applications and static sites using the JAMStack.</p>
+            <div class="flex flex-col items-start ml-6 mr-6 mb-3">
+                <p class="flex flex-col items-start font-sans text-xl text-green-700 font-semibold">Interests</p>   
+            </div>
+            
+            <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-xl text-gray-300 font-normal">Developing progressive web applications, single page applications and static sites using the JAMStack.</p>
             
             <p class="flex flex-col items-start ml-6 mr-6 mb-3 font-sans text-base text-gray-300 font-normal">My current interests and studies include Javascript, ES6 and the MEVN and MERN Stacks. You might find me poking around in documentation for the following frameworks: NodeJS, ExpressJS, MongoDB, AngularJS, VueJS, ReactJS, NativeScript-Vue, React-Native, NuxtJS, Gridsome, GraphQL, TailwindCSS, Flexbox, CSS Grid, SASS, LESS and Ionic Hybrid Mobile Framework.</p>
     
