3efdcb21be05b2ea08bd7cc82da1b1a446797bcc
steve-visibble <steveobrien73@gmail.com>
2019-10-12 18:01:20 -0400

    edit

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -12,11 +12,39 @@
         <div class="repo-item flex flex-col items-center border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-t rounded-b lg:rounded-b-none lg:rounded-r p-4 m-6"> 
             <g-image src="~/assets/images/git-repo-link.png" width="500"/>
             <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">ITP Deployment App</div>
+                <div class="font-bold text-xl mb-2 text-center">GRIDSOME APP</div>
+                <p class="text-gray-700 text-base">
+                This App is an example portfolio site for information about myself. Gridsome is a static site generator built using VueJS GrapgQL and TailwindCSS. Lots of cool features. 
+                </p>
+            </div>
+            <div class="repo-btn flex flex-row flex-no-wrap">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-6"><a href="https://github.com/sobrien172513/gridsome-portfolio" target="_blank">REPO</a></button>
+                
+                <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"><a href="https://www.stephenobrien.me" target="_blank">RUN</a></button>
+            </div>
+            <div class="px-6 py-4">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">gridsome</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">vuejs</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">graphql</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">tailwindcss</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">netlify</span>
+            </div>
+        </div>
+        
+        <div class="repo-item flex flex-col items-center border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-t rounded-b lg:rounded-b-none lg:rounded-r p-4 m-6"> 
+            <g-image src="~/assets/images/git-repo-link.png" width="500"/>
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2 text-center">ITP Deployment App</div>
                 <p class="text-gray-700 text-base">
                 This project was intended to help scientific research. It is a demo created to control weather testing devices at sea near the north pole.
                 </p>
             </div>
+            <div class="repo-btn flex flex-row flex-no-wrap">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-6"><a href="https://github.com/sobrien172513/itp-deployment-tool" target="_blank">REPO</a></button>
+                
+                <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"><a href="https://itp-deployment-tool.herokuapp.com" target="_blank">RUN</a></button>
+            </div>
+            
             <div class="px-6 py-4">
                 <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">python</span>
                 <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">bottle</span>
@@ -24,7 +52,7 @@
                 <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">javascript</span>
                 <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">bootstrap</span>
             </div>
-        </div>   
+        </div> 
 
     </div>
   </Layout>
