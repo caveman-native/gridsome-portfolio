11b08e2da43c9f940842658c31b9eb1564999579
sobrien172513 <steveobrien73@gmail.com>
2019-10-13 19:13:09 -0400

    edit

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -16,7 +16,7 @@
                 </label>
                 </div>
                 <div class="md:w-2/3">
-                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="name" onfocus="this.placeholder=''" onblur="this.placeholder=''" value="">
+                <input name="name" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="name" onfocus="this.placeholder=''" onblur="this.placeholder=''" value="">
                 </div>
             </div>
             <div class="md:flex md:items-center mb-6">
@@ -26,7 +26,7 @@
                 </label>
                 </div>
                 <div class="md:w-2/3">
-                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-email-address" type="email" placeholder="email" onfocus="this.placeholder=''" onblur="this.placeholder=''" value="">
+                <input name="email" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-email-address" type="email" placeholder="email" onfocus="this.placeholder=''" onblur="this.placeholder=''" value="">
                 </div>
             </div>
             <div class="md:flex md:items-center mb-6">
@@ -42,7 +42,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3"></div>
                 <label class="md:w-2/3 block text-gray-500 font-bold">
-                <input class="mr-2 leading-tight" type="checkbox">
+                <input name="checkbox" class="mr-2 leading-tight" type="checkbox">
                 <span class="text-sm">
                     Get back to me asap. It's about a job!
                 </span>
