b151d88bb14c50daa121c6b3b0d4f22f5c3f5bc7
sobrien172513 <steveobrien73@gmail.com>
2020-08-10 20:55:12 -0400

    edits

--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -15,7 +15,7 @@
                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                 <a href="https://www.stephenobrien.me/Steve.OBrien.072420.pdf" download="Steve.OBrien.072420.pdf">download pdf</a>
             </button>
-            <!-- staging push and netlify build test -->
+            <!-- staging push and netlify build test zzz -->
             
             
         </div>
